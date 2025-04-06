7ce69706ac4c2cf6c65624c43c0eb13edcff4a01
JonathanJirefalk <3tomu24.jirejo@folkuniversitetet.nu>
2025-04-06 22:41:10 +0200

    File Error Fixed

--- Recensioner/Recensioner/src/main/java/com/example/Recensioner/Repository/RecensionRepository.java
+++ Recensioner/Recensioner/src/main/java/com/example/Recensioner/Repository/RecensionRepository.java
@@ -1,11 +1,12 @@
 package com.example.Recensioner.Repository;
 
+import com.example.Recensioner.entity.Recension;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Recensioner.entity.Recension;
-
+@Repository
 public interface RecensionRepository extends JpaRepository<Recension, Long> {
     List<Recension> findByUserId(Long userId);
 }
